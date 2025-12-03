c7d04c6a751350b7e23a87bfdfeb2e05088520e0
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-03 17:00:18 +0700

    update alb

--- infra/modules/ingress/main.tf
+++ infra/modules/ingress/main.tf
@@ -32,90 +32,6 @@ locals {
 }
 
 # Deployments for each service
-resource "kubernetes_deployment_v1" "api" {
-  for_each = { for s in local.services : s.name => s }
-
-  metadata {
-    name      = each.key
-    namespace = kubernetes_namespace_v1.apps.metadata[0].name
-    labels = {
-      app = each.key
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = each.key
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = each.key
-        }
-      }
-
-      spec {
-        container {
-          name  = each.key
-          image = each.value.image
-
-          port {
-            container_port = 8080
-          }
-
-          readiness_probe {
-            http_get {
-              path = "/health"
-              port = 8080
-            }
-            initial_delay_seconds = 3
-            period_seconds        = 10
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/health"
-              port = 8080
-            }
-            initial_delay_seconds = 10
-            period_seconds        = 20
-          }
-        }
-      }
-    }
-  }
-}
-
-# ClusterIP services
-resource "kubernetes_service_v1" "api" {
-  for_each = kubernetes_deployment_v1.api
-
-  metadata {
-    name      = each.key
-    namespace = each.value.metadata[0].namespace
-    labels = {
-      app = each.key
-    }
-  }
-
-  spec {
-    selector = {
-      app = each.key
-    }
-
-    port {
-      port        = 80
-      target_port = 8080
-    }
-
-    type = "ClusterIP"
-  }
-}
 resource "kubernetes_ingress_v1" "apps_ingress" {
   wait_for_load_balancer = true
 
@@ -124,10 +40,11 @@ resource "kubernetes_ingress_v1" "apps_ingress" {
     namespace = kubernetes_namespace_v1.apps.metadata[0].name
 
     annotations = {
+      "kubernetes.io/ingress.class"               = "alb"
       "alb.ingress.kubernetes.io/scheme"          = "internet-facing"
-      "alb.ingress.kubernetes.io/certificate-arn" = var.acm_certificate_arn
-      "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTPS\":443}]"
       "alb.ingress.kubernetes.io/target-type"     = "ip"
+      "alb.ingress.kubernetes.io/listen-ports"    = "[{\"HTTPS\":443}]"
+      "alb.ingress.kubernetes.io/certificate-arn" = var.acm_certificate_arn
     }
   }
 
@@ -135,16 +52,16 @@ resource "kubernetes_ingress_v1" "apps_ingress" {
     ingress_class_name = "alb"
 
     rule {
-      host = local.full_domain
+      host = "api.${var.subdomain}.${var.domain}"
 
       http {
         path {
-          path = "/a"
+          path      = "/a"
           path_type = "Prefix"
 
           backend {
             service {
-              name = kubernetes_service_v1.service_a.metadata[0].name
+              name = "service-a"
               port {
                 number = 80
               }
@@ -153,12 +70,12 @@ resource "kubernetes_ingress_v1" "apps_ingress" {
         }
 
         path {
-          path = "/b"
+          path      = "/b"
           path_type = "Prefix"
 
           backend {
             service {
-              name = kubernetes_service_v1.service_b.metadata[0].name
+              name = "service-b"
               port {
                 number = 80
               }
@@ -167,12 +84,12 @@ resource "kubernetes_ingress_v1" "apps_ingress" {
         }
 
         path {
-          path = "/c"
+          path      = "/c"
           path_type = "Prefix"
 
           backend {
             service {
-              name = kubernetes_service_v1.service_c.metadata[0].name
+              name = "service-c"
               port {
                 number = 80
               }
@@ -183,4 +100,3 @@ resource "kubernetes_ingress_v1" "apps_ingress" {
     }
   }
 }
-
