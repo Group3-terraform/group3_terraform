b62233f2847b436c07192f25913c1afba7ec8a02
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-03 15:25:08 +0700

    fix iam polict

--- infra/modules/iam/outputs.tf
+++ infra/modules/iam/outputs.tf
@@ -5,9 +5,11 @@ output "cluster_role_arn" {
 output "node_role_arn" {
   value = aws_iam_role.eks_node_role.arn
 }
+
+output "node_role_name" {
+  value = aws_iam_role.eks_node_role.name
+}
+
 output "lb_controller_policy_arn" {
   value = aws_iam_policy.aws_load_balancer_controller_policy.arn
-}
-output "node_role_name" {
-  value = aws_iam_role.node.name
 }
