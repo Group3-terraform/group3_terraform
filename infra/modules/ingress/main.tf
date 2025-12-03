1cd55521c6a4aa0e89adb9eef78381b1a2cd4a9b
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-03 18:23:22 +0700

    fix dev

--- infra/modules/ingress/main.tf
+++ infra/modules/ingress/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role" "alb_controller" {
 
 resource "aws_iam_role_policy_attachment" "alb_policy_attach" {
   role       = aws_iam_role.alb_controller.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+  policy_arn = "arn:aws:iam::570430250751:policy/AWSLoadBalancerControllerIAMPolicy"
 }
 
 ###############################
