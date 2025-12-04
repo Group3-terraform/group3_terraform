db45f05ed32d7705b2acad9cbe256313ade401d1
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-04 16:36:38 +0700

    update

--- infra/envs/dev/provider.tf
+++ infra/envs/dev/provider.tf
@@ -50,12 +50,7 @@ provider "kubernetes" {
       "--cluster-name", module.eks.cluster_name
     ]
   }
-
-  depends_on = [
-    module.eks
-  ]
 }
-
 
 #########################################
 # Helm Provider (for aws-load-balancer-controller)
