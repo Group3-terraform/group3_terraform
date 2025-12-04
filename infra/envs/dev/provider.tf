cef754a4658165215aa76672bd37140ec02cba4d
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-04 16:44:18 +0700

    update

--- infra/envs/dev/provider.tf
+++ infra/envs/dev/provider.tf
@@ -24,9 +24,6 @@ terraform {
   required_version = ">= 1.6.0"
 }
 
-provider "aws" {
-  region = var.aws_region
-}
 
 # This uses the EKS cluster created by module.eks
 #########################################
