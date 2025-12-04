31204a5da8309be68c76756fdee42d96d365a29c
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-04 16:02:57 +0700

    update

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -53,23 +53,23 @@ module "eks" {
 # Kubernetes Provider (AFTER EKS READY)
 #########################################
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_ca)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = [
-      "eks",
-      "get-token",
-      "--region", var.aws_region,
-      "--cluster-name", module.eks.cluster_name
-    ]
-  }
+#   exec {
+#     api_version = "client.authentication.k8s.io/v1beta1"
+#     command     = "aws"
+#     args        = [
+#       "eks",
+#       "get-token",
+#       "--region", var.aws_region,
+#       "--cluster-name", module.eks.cluster_name
+#     ]
+#   }
 
-  depends_on = [module.eks]
-}
+#   depends_on = [module.eks]
+# }
 
 
 #########################################
