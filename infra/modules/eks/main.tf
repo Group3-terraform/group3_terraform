0a68a0fcbe29af992b6b1f429e34a63bd8907656
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-05 10:56:25 +0700

    update eks

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,3 +1,11 @@
+############################################
+# Get Current IAM Identity
+############################################
+data "aws_caller_identity" "current" {}
+
+############################################
+# EKS Cluster
+############################################
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.37"
@@ -5,17 +13,51 @@ module "eks" {
   cluster_name    = "${var.project_name}-${var.environment}-eks"
   cluster_version = var.cluster_version
 
+  vpc_id     = var.vpc_id
+  subnet_ids = var.private_subnets
+
   enable_irsa = true
 
-  iam_role_arn = var.iam_role_arn
-  vpc_id       = var.vpc_id
-  subnet_ids   = var.private_subnets
+  ############################################
+  # Cluster API Endpoint Options
+  ############################################
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = true
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
-  ## FIX: Terraform must reach Kubernetes API
-  cluster_endpoint_public_access        = true
-  cluster_endpoint_private_access       = true
-  cluster_endpoint_public_access_cidrs  = ["0.0.0.0/0"]
+  ############################################
+  # IAM Roles for Cluster & Nodes
+  ############################################
+  iam_role_arn      = var.iam_role_arn
+  node_iam_role_arn = var.node_iam_role_arn
 
+  ############################################
+  # ADD THIS: RBAC FIX – Give Terraform Access
+  ############################################
+  manage_aws_auth_configmap = true
+
+  aws_auth_users = [
+    {
+      userarn  = data.aws_caller_identity.current.arn
+      username = "terraform-admin"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  aws_auth_roles = [
+    {
+      rolearn  = var.node_iam_role_arn
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups = [
+        "system:bootstrappers",
+        "system:nodes"
+      ]
+    }
+  ]
+
+  ############################################
+  # Node Group
+  ############################################
   eks_managed_node_groups = {
     default = {
       min_size     = var.node_min
