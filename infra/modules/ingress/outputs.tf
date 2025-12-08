f981be0eb5004e4d285c1b4268b48804cf586ee7
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-08 11:44:28 +0700

    update alb_dns_name outputs

--- infra/modules/ingress/outputs.tf
+++ infra/modules/ingress/outputs.tf
@@ -3,8 +3,13 @@ output "ingress_hostname" {
 }
 
 output "alb_dns_name" {
-  value = kubernetes_ingress_v1.apps_ingress.status[0].load_balancer[0].ingress[0].hostname
+  value = (
+    length(kubernetes_ingress_v1.apps_ingress.status) > 0 &&
+    length(kubernetes_ingress_v1.apps_ingress.status[0].load_balancer) > 0 &&
+    length(kubernetes_ingress_v1.apps_ingress.status[0].load_balancer[0].ingress) > 0
+  ) ? kubernetes_ingress_v1.apps_ingress.status[0].load_balancer[0].ingress[0].hostname : ""
 }
+
 
 output "alb_zone_id" {
   value = "Z1LMS91P8CMLE5"
