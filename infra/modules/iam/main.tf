f69ec344f6c64721da0182d8505c8269ca39e935
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-10 13:59:18 +0700

    update item new v2

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,7 +1,32 @@
 ############################################
-# DATA: Get AWS Account ID
+# DATA: AWS Account ID
 ############################################
 data "aws_caller_identity" "current" {}
+
+############################################
+# DATA: EKS Cluster & OIDC Information
+############################################
+data "aws_eks_cluster" "eks" {
+  name = "${var.project_name}-${var.environment}-eks"
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = data.aws_eks_cluster.eks.name
+}
+
+locals {
+  oidc_url      = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  oidc_hostpath = replace(local.oidc_url, "https://", "")
+}
+
+############################################
+# IAM OIDC Provider (IRSA)
+############################################
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = local.oidc_url
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd80e35"]
+}
 
 ############################################
 # EKS Cluster IAM Role
@@ -62,16 +87,6 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly" {
 ############################################
 # ALB Controller IAM Role (IRSA)
 ############################################
-# We use a SAFE wildcard OIDC trust because every EKS cluster automatically 
-# creates an OIDC provider like:
-# arn:aws:iam::<account>:oidc-provider/oidc.eks.<region>.amazonaws.com/id/<random>
-#
-# This avoids passing OIDC provider ARN/URL into IAM module.
-############################################
-
-############################################
-# ALB Controller IAM Role (IRSA)
-############################################
 resource "aws_iam_role" "alb_controller_role" {
   name = "${var.project_name}-${var.environment}-alb-controller-role"
 
@@ -80,21 +95,22 @@ resource "aws_iam_role" "alb_controller_role" {
     Statement = [{
       Effect = "Allow"
       Principal = {
-        Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/oidc.eks.ap-southeast-1.amazonaws.com/id/FDE17E8C655C464CA059FBEF72FFEB2A"
+        Federated = aws_iam_openid_connect_provider.eks.arn
       }
       Action = "sts:AssumeRoleWithWebIdentity"
       Condition = {
         StringEquals = {
-          "oidc.eks.ap-southeast-1.amazonaws.com/id/FDE17E8C655C464CA059FBEF72FFEB2A:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          "${local.oidc_hostpath}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller",
+          "${local.oidc_hostpath}:aud" = "sts.amazonaws.com"
         }
       }
     }]
   })
 }
 
-
-
-
+############################################
+# ALB Controller IAM Policy
+############################################
 resource "aws_iam_policy" "alb_controller_policy" {
   name   = "${var.project_name}-${var.environment}-alb-controller-policy"
   policy = file("${path.module}/iam_policy.json")
@@ -103,4 +119,19 @@ resource "aws_iam_policy" "alb_controller_policy" {
 resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
   role       = aws_iam_role.alb_controller_role.name
   policy_arn = aws_iam_policy.alb_controller_policy.arn
+}
+
+############################################
+# Outputs
+############################################
+output "cluster_role_arn" {
+  value = aws_iam_role.eks_cluster_role.arn
+}
+
+output "node_role_arn" {
+  value = aws_iam_role.eks_node_role.arn
+}
+
+output "alb_controller_role_arn" {
+  value = aws_iam_role.alb_controller_role.arn
 }
