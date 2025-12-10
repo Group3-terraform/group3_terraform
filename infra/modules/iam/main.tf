205198073e0549c88a2a5816e14bfef2c8928b60
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-10 14:13:16 +0700

    update iam

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -22,11 +22,11 @@ locals {
 ############################################
 # IAM OIDC Provider (IRSA)
 ############################################
-resource "aws_iam_openid_connect_provider" "eks" {
-  url             = local.oidc_url
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd80e35"]
-}
+# resource "aws_iam_openid_connect_provider" "eks" {
+#   url             = local.oidc_url
+#   client_id_list  = ["sts.amazonaws.com"]
+#   thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd80e35"]
+# }
 
 ############################################
 # EKS Cluster IAM Role
