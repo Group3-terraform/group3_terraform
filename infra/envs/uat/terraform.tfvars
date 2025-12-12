a719e454a7960ae1b5ff9931df538690994522b9
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-12 15:49:01 +0700

    add uat

--- infra/envs/uat/terraform.tfvars
+++ infra/envs/uat/terraform.tfvars
@@ -1,1 +1,40 @@
-environment = "uat"
+project_name = "group3"
+environment  = "uat"
+
+cluster_version = "1.34"
+
+azs = [
+  "ap-southeast-1a",
+  "ap-southeast-1b",
+]
+
+public_subnets = [
+  "10.0.0.0/20",
+  "10.0.16.0/20",
+]
+
+private_subnets = [
+  "10.0.32.0/20",
+  "10.0.48.0/20",
+]
+
+hosted_zone_id  = "Z07852252OWMU8O090PPL"
+
+zone_id = "Z07852252OWMU8O090PPL"
+
+domain          = "theareak.click"
+subdomain       = "api.uat"
+
+node_min     = 1
+node_desired = 1
+node_max     = 2
+
+
+aws_region = "ap-southeast-1"
+
+
+tls_secret_name = "theareak-tls"
+
+service_a_image = "570430250751.dkr.ecr.ap-southeast-1.amazonaws.com/service-a:dev-v1.0.16"
+service_b_image = "570430250751.dkr.ecr.ap-southeast-1.amazonaws.com/service-b:dev-v1.0.12"
+service_c_image = "570430250751.dkr.ecr.ap-southeast-1.amazonaws.com/service-c:dev-v1.0.12"
---
+++ infra/envs/uat/variables.tf
@@ -0,0 +1,83 @@
+###############################
+# Global vars
+###############################
+
+variable "project_name" {}
+variable "environment"  {}
+
+variable "aws_region" {
+  type    = string
+  default = "ap-southeast-1"
+}
+
+variable "cluster_version" {
+  type    = string
+  default = "1.34"
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "node_min" {
+  type = number
+}
+
+variable "node_desired" {
+  type = number
+}
+
+variable "node_max" {
+  type = number
+}
+
+###############################
+# Domain / DNS / ACM
+###############################
+
+variable "domain" {
+  type = string
+}
+
+variable "subdomain" {
+  type = string
+  # For uat this will be "api.uat"
+}
+
+variable "hosted_zone_id" {
+  description = "Route53 Hosted Zone ID for theareak.click"
+  type        = string
+}
+
+###############################
+# (Optional) image & TLS vars you already had
+###############################
+
+variable "tls_secret_name" {
+  type = string
+}
+
+variable "service_a_image" {
+  type = string
+}
+
+variable "service_b_image" {
+  type = string
+}
+
+variable "service_c_image" {
+  type = string
+}
+
+variable "zone_id" {
+  type = string
+  description = "Deprecated: use hosted_zone_id instead"
+}
