f89872bea8a640350a6fb98b10917e271e63e05c
Sam Vutheareak <sam.vutheareak@FA-993-601-1082.local>
2025-12-13 15:04:57 +0700

    fix update

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -25,20 +25,20 @@ data "aws_caller_identity" "current" {}
 ############################################
 # EKS Cluster IAM Role
 ############################################
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.project_name}-${var.environment}-eks-cluster-role"
+# resource "aws_iam_role" "eks_cluster_role" {
+#   name = "${var.project_name}-${var.environment}-eks-cluster-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "eks.amazonaws.com"
+#       }
+#       Action = "sts:AssumeRole"
+#     }]
+#   })
+# }
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
